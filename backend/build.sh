132f281ac0622a6f39cc87768923aa09b3845743
Muthamizhan-N <muthamizhann2006@gmail.com>
2026-07-13 20:22:37 +0530

    service fix

--- backend/build.sh
+++ backend/build.sh
@@ -6,3 +6,6 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Automatically create/reset the admin account in the live database
+python setup_admin.py
