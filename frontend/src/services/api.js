254414f469d689550a3282e1812b1cbf837355b5
Muthamizhan-N <muthamizhann2006@gmail.com>
2026-07-13 20:16:23 +0530

    Api push

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API_BASE = 'http://localhost:8000/api';
+const API_BASE = import.meta.env.VITE_API_URL 
+  ? `${import.meta.env.VITE_API_URL.replace(/\/$/, '')}/api`
+  : 'http://localhost:8000/api';
 
 // Create Axios Instance
 const client = axios.create({
